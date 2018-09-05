e1e4290360d77c9cf02caec6e2523f9d242be5a3
jamesford <jamie@stobbs.me>
2018-09-05 16:31:42 +0200

    Move starter text into js script tag

--- src/Marker.elm
+++ src/Marker.elm
@@ -53,26 +53,6 @@ view m =
     ]
 
 
--- UTIL
-
-starter : String
-starter = """# This is Markdown
-
-[Markdown](http://daringfireball.net/projects/markdown/) lets you write content in a really natural way.
-
-  * You can have lists, like this one
-  * Make things **bold** or *italic*
-  * Embed snippets of `code`
-  * Create [links](/)
-  * ...
-
-The [elm-markdown][] package parses all this content, allowing you
-to easily generate blocks of `Element` or `Html`.
-
-[elm-markdown]: http://package.elm-lang.org/packages/evancz/elm-markdown/latest
-"""
-
-
 -- PORTS
 
 port save : String -> Cmd msg
